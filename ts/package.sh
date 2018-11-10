293860fddf88eb8992f0ebc3e08698cd9237b864
streamich <streamich@gmail.com>
2018-11-10 17:26:48 +0100

    feat: 🎸 add package.sh to generate package.json files

---
+++ ts/package.sh
@@ -0,0 +1,39 @@
+echo "Adding package.json"
+
+# Default GitHub username to use if one not set.
+DEFAULT_USERNAME="streamich"
+
+# Fetch CLI named params.
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+      -u|--username)
+      USERNAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+  esac
+done
+set -- "${POSITIONAL[@]}"
+
+if [ -z ${USERNAME+x} ]; then
+  echo "--username not set, will use default username: '$DEFAULT_USERNAME'.";
+  USERNAME=$DEFAULT_USERNAME;
+fi
+
+echo "Using GitHub username: '$USERNAME'.";
+
+# Check mrm is installed.
+if ! [ -x "$(command -v mrm)" ]; then
+  echo 'Error: mrm is not installed, run: yarn global add mrm.' >&2
+  echo 'See <https://github.com/sapegin/mrm>' >&2
+  exit 1
+fi
+
+GITHUB_URL="https://github.com/$USERNAME"
+
+mrm package \
+  --config:url $GITHUB_URL \
+  --config:github "$USERNAME"
