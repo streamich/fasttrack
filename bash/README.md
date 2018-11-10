16523ce5c55c436efa763358f6118e60210e9fc8
streamich <streamich@gmail.com>
2018-11-10 20:10:38 +0100

    feat: 🎸 improve git script

---
+++ bash/README.md
@@ -0,0 +1,11 @@
+This is collection of scripts you can call with
+
+```shell
+fasttrack-cli <script>
+```
+
+To see help information for some script run
+
+```shell
+fasttrack-cli <script> --help
+```
--- bash/git.sh
+++ bash/git.sh
@@ -1,4 +1,44 @@
-# Add Git.
+#!/usr/bin/env bash
+
+# Adding Git.
+
+# Default GitHub username to use if one not set.
+DEFAULT_USERNAME="streamich"
+HELP=false
+
+# Fetch CLI named params.
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+      -u|--username)
+      USERNAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+  esac
+done
+set -- "${POSITIONAL[@]}"
+
+if [ "$HELP" = true ] ; then
+  echo "fasttrack-cli git [options]"
+  echo ""
+  echo "    Init Git repo and create first commit"
+  echo ""
+  echo "Options"
+  echo "    --username        username to use in Git, defaults to 'streamich'"
+  echo "    -h, --help        show this output"
+  exit 0
+fi
+
+if [ -z ${USERNAME+x} ]; then
+  echo "--username not set, will use default username: '$DEFAULT_USERNAME'.";
+  USERNAME=$DEFAULT_USERNAME;
+fi
+
+echo "Using GitHub username: '$USERNAME'.";
+
 echo "Adding Git"
 git init
 git config user.name "$USERNAME"
