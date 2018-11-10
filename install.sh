91c357a94b35f7efff5f3925d177e4634a41cfc9
streamich <streamich@gmail.com>
2018-11-10 23:40:05 +0100

    feat: 🎸 create ft alias for fasttrack command

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ BIN_FOLDER="/usr/local/bin"
 FASTTRACK="https://raw.githubusercontent.com/streamich/fasttrack/master/fasttrack"
 
 curl -o $BIN_FOLDER/fasttrack $FASTTRACK
+cp $BIN_FOLDER/fasttrack $BIN_FOLDER/ft
 chmod +x $BIN_FOLDER/fasttrack
+chmod +x $BIN_FOLDER/ft
