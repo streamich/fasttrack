79de36ee410f229cfdda7226d4bd9aabc082a320
Va Da <streamich@users.noreply.github.com>
2018-11-25 18:15:32 +0100

    fix: use correct dir in package.json scripts

--- scripts/build-ts.sh
+++ scripts/build-ts.sh
@@ -50,11 +50,11 @@ if ! [ -x "$(command -v climod-json)" ]; then
 fi
 
 echo "Adding build and clean scripts."
-climod-add-script --name clean --cmd "rimraf $DIST"
+climod-add-script --name clean --cmd "rimraf $DIR_DIST"
 climod-add-script --name build --cmd "tsc"
 
 echo "Configuring distribution files."
-climod-json --file package.json --key files --set "[\"${DIST}/\"]" --json
+climod-json --file package.json --key files --set "[\"${DIR_DIST}/\"]" --json
 climod-json --file package.json --key main --set "lib/index.js"
 climod-json --file package.json --key types --set "lib/index.d.ts"
 climod-json --file package.json --key typings --set "lib/index.d.ts"
